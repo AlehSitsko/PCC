9326137712d600cafc987d5d7a571581af55140c
AlehSitsko <149909434+AlehSitsko@users.noreply.github.com>
2025-09-10 12:20:15 -0400

    Some part of part 6 Dictionaries done

---
+++ CrashCourse/stydying/Dictionaries/6-1.py
@@ -0,0 +1,14 @@
+# Storing a person in a dictionary
+person = {
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'age': 30,
+    'city': 'New York',
+}
+# Printing the dictionary
+print(person)
+# Printing each value in the dictionary
+print(f"First Name: {person['first_name']}")
+print(f"Last Name: {person['last_name']}")
+print(f"Age: {person['age']}")
+print(f"City: {person['city']}")
