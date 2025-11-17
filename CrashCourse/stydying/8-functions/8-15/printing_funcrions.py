b1444c65a703679b926a21122020048c1d7f0fd7
AlehSitsko <149909434+AlehSitsko@users.noreply.github.com>
2025-11-17 16:45:19 -0500

    some part 8 added

---
+++ CrashCourse/stydying/8-functions/8-15/printing_funcrions.py
@@ -0,0 +1,10 @@
+# Printing functions
+def test_printing():
+    print("Hello, World!")
+    print("This is a test of printing functions.")
+    print("Printing multiple lines:")
+    for i in range(3):
+        print(f"Line {i + 1}")
+
+# Call the test function
+test_printing()
---
+++ CrashCourse/stydying/8-functions/8-15/printing_models.py
@@ -0,0 +1,4 @@
+# importing the printing functions
+from printing_funcrions import test_printing as pf
+# Call the imported printing function
+pf()
